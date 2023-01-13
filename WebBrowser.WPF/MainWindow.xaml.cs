4bc46bb0ce32042f98e7c9f06c1341ad85655d22
jeromemanzano <jeromemanzano@gmail.com>
2023-01-14 07:12:10 +0800

    Feature/create solution (#8)
    * Add WPF project
    * Add Core project
    * Add Test Project

---
+++ WebBrowser.WPF/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ReactiveUI;
+using WebBrowser.ViewModels;
+
+namespace WebBrowser.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ReactiveWindow<MainViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
