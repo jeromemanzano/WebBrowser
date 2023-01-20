ede34a8e3a4dcd14542fef3b5ff3297a4d12d0da
Jerome Manzano <jeromemanzano@gmail.com>
2023-01-21 07:12:00 +0800

    Handle AddressBarText up and down key in MainWindow code behind

--- WebBrowser.WPF/MainWindow.xaml.cs
+++ WebBrowser.WPF/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using ReactiveUI;
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ReactiveUI;
 using Splat;
-using WebBrowser.Services;
 using WebBrowser.ViewModels;
 
 namespace WebBrowser.WPF
@@ -10,12 +13,45 @@ namespace WebBrowser.WPF
     /// </summary>
     public partial class MainWindow : ReactiveWindow<MainViewModel>
     {
+        private readonly ListBox _suggestionsListBox;
+        private readonly TextBox _addressBarTextBox;
+        
         public MainWindow()
         {
             InitializeComponent();
             var viewModel = Locator.Current.GetService<MainViewModel>();
             DataContext = viewModel;
             ViewModel = viewModel;
+            _suggestionsListBox = (ListBox)FindName("SuggestionsListBox");
+            _addressBarTextBox = (TextBox)FindName("AddressBarTextBox");
+        }
+        
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            Akavache.BlobCache.Shutdown().Wait();
+        }
+
+        // We are handling it here since this is probably not needed in other platforms
+        private void OnTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Down && _suggestionsListBox.SelectedIndex < _suggestionsListBox.Items.Count - 1)
+            {
+                _suggestionsListBox.SelectedIndex++;
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Up && _suggestionsListBox.SelectedIndex > 0)
+            {
+                _suggestionsListBox.SelectedIndex--;
+                e.Handled = true;
+            }
+            else
+            {
+                return;
+            }
+            
+            _addressBarTextBox.Text = _suggestionsListBox.SelectedItem.ToString()!;
+            _addressBarTextBox.CaretIndex = _addressBarTextBox.Text.Length;
         }
     }
 }
